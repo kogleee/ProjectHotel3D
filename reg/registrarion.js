97e7ead8d107ba67ed8dbfdf2fc34faf04b2a500
kogleee <137287221+kogleee@users.noreply.github.com>
2024-02-29 11:16:16 +0300

    Add files via upload

--- reg/registrarion.js
+++ reg/registrarion.js
@@ -1,11 +1,35 @@
+//анимации
 let reg = document.querySelector("#reg")
-let login = document.querySelector("#log")
+let log = document.querySelector("#log")
 let container = document.querySelector(".container")
 
 reg.addEventListener("click", ()=>{
     container.classList.add("active")
 })
 
-login.addEventListener("click", ()=>{
+log.addEventListener("click", ()=>{
     container.classList.remove("active")
 })
+
+
+let phoneReg = document.querySelector("#phone-reg")
+let phoneLog = document.querySelector("#phone-log")
+
+let regPanel = document.querySelector(".left")
+let logPanel = document.querySelector(".right")
+
+phoneReg.addEventListener("click", ()=>{
+    regPanel.style.display = "flex";
+    logPanel.style.display = "none";
+})
+
+phoneLog.addEventListener("click", ()=>{
+    regPanel.style.display = "none";
+    logPanel.style.display = "flex";
+})
+
+//валидация и отправка данных
+let registration = document.querySelector("#registration")
+let login = document.querySelector("#login")
+
+
